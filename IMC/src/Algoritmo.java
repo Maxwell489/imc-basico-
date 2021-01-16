1d4869c5060aacdeed40639a914b0ab981b8cdf4
Maxwell <69826121+Maxwell489@users.noreply.github.com>
2021-01-16 01:48:31 -0300

    Add files via upload

---
+++ IMC/src/Algoritmo.java
@@ -0,0 +1,14 @@
+Algoritmo numeros
+	int nota	
+
+escreva ("digite a primeira nota")
+	leia("nota1")
+escreva ("digite a segunda nota")
+	leia("nota2")
+if {	nota1>=nota2
+	escreva("nota 1 maior q nota 2")
+}
+else{ 
+	escreva("nota 2 maior q nota 1")
+}
+fim
---
+++ IMC/src/imc/IMC.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imc;
+
+/**
+ *
+ * @author logonpta
+ */
+public class IMC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Algoritmo Salario
+	real SalarioPessoa, Percentual, Aumento, NovoSalario
+ 
+início
+escreva (“Digite o seu Sálario”)
+	leia (SalarioPessoa)
+escreva (“Digite o Percentual de aumento”)
+	leia (Percentual)
+Aumento = (NovoSalario * (Percentual/100))
+	leia (Aumento)
+NovoSalario = (SalarioPessoa + Aumento)
+	leia (NovoSalario)
+escreva (“Seu Sálario atual é de “ + SalarioPessoa + “, com o Percentual de “ + Percentual
++ "terá aumento de " Aumento + “, e seu novo salario passará a ser “ + NovoSalario ".")
+fim
+    
+}
